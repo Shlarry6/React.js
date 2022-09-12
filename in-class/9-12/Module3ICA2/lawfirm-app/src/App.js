d91e5d53fade968026ec33fa26a0ba03b19d4d15
Michael Sears <A01657265@aggies.usu.edu>
2022-09-12 11:43:15 -0600

    Lawfirm app for part 2 of in class assignment added.

---
+++ in-class/9-12/Module3ICA2/lawfirm-app/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <div>placeholder</div>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
