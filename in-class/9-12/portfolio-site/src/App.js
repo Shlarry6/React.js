7693e6f817459e040992aa2d18b78378e313fc8c
Michael Sears <A01657265@aggies.usu.edu>
2022-09-12 10:32:52 -0600

    9-12 inclass excersize added. have not yet built upon previous lesson yet

---
+++ in-class/9-12/portfolio-site/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import GoalList from './components/Goals/GoalList';
+import Header from './components/UI/Header';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <GoalList />
+    </div>
+  );
+}
+
+export default App;
